f5bd1a916746b9476824f5102e2c2cb164c2c890
Aeveron <33118787+Aeveron@users.noreply.github.com>
2018-11-08 14:08:19 +0100

    x
    x

---
+++ src/components/Names.vue
@@ -0,0 +1,25 @@
+<template>
+
+<div class="components">
+  <p>Test</p>
+  <p>{{ msg }}</p>
+
+</div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      msg: 'HEISANN!',
+      name: 'Names'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  p {
+    color: blue;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="components">
+    <input type="button" value="Hei" id="btn1"></input>
+  <names></names>
+  </div>
+</template>
+
+<script>
+import Names from '@/components/Names.vue';
+
+export default {
+  name: 'Names',
+  components: {
+    Names
+  }
+}
+</script>
+
+<style scoped>
+#btn1{
+  padding: 1%;
+}
+</style>
+
